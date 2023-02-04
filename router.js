5e09481b3b598ac256d98010fbb305706090de6b
partha-sarkar <parthasarkaremail@gmail.com>
2023-02-05 00:36:58 +0530

    m

--- router.js
+++ router.js
@@ -6,20 +6,21 @@ const authMiddleware = require('./middleware/auth.middleware');
 const users = require('./controller/users.controller');
 
 
-router.get('/',(req,res)=>{
-    res.send('hello-1');
-    console.log('root');
+router.get('/',authMiddleware,(req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({"status":200})); 
+
 });
 
 
-
+//login POST
 router.post('/login',async (req,res)=>{
     try{
-    const tempObjUser = await users.loginSubmit(req.body);
-    let ObjUser = JSON.parse(tempObjUser);
-    res.setHeader('Content-Type', 'application/json');
-    res.status(ObjUser.status);
-    res.send(ObjUser); 
+        const tempObjUser = await users.loginSubmit(req.body);
+        let ObjUser = JSON.parse(tempObjUser);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(ObjUser.status);
+        res.send(ObjUser);
 
     }catch (err) {
         console.log(err);
